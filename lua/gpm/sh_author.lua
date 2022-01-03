1a3df9a2ba3afcd47aad81473f1ee232e717148c
Retro <44505837+dankmolot@users.noreply.github.com>
2022-01-03 19:43:29 +0200

    Fixed author parsing (messed up url and email)

--- lua/gpm/sh_author.lua
+++ lua/gpm/sh_author.lua
@@ -3,13 +3,13 @@ GPM.Author = GPM.Author or {}
 
 local function parseAuthor(str)
     local name = string.match(str, '^(.-)%s?[<%(]') or string.match(str, '.+')
-    local url = string.match(str, '<(.-)>')
-    local email = string.match(str, '%((.-)%)')
+    local email = string.match(str, '<(.-)>')
+    local url = string.match(str, '%((.-)%)')
 
     return {
         name = name,
-        url = url,
-        email = email
+        email = email,
+        url = url
     }
 end
 
@@ -19,14 +19,14 @@ mt.__index = mt
 function mt:__eq(other)
     return istable(other) and
         self.name == other.name and
-        self.url == other.url and
-        self.email == other.email
+        self.email == other.email and
+        self.url == other.url
 end
 
 function mt:__tostring()
 	local buffer = { self.name }
-	if self.url then buffer[#buffer+1] = ' <' .. self.url .. '>' end
-	if self.email then buffer[#buffer+1] = ' (' .. self.email .. ')' end
+	if self.email then buffer[#buffer+1] = ' <' .. self.url .. '>' end
+	if self.url then buffer[#buffer+1] = ' (' .. self.email .. ')' end
 	return table.concat(buffer)
 end
 
@@ -41,8 +41,8 @@ function mt.new(v)
 
     return setmetatable({
         name = v.name,
-        url = v.url,
-        email = v.email
+        email = v.email,
+        url = v.url
     }, mt)
 end
 
