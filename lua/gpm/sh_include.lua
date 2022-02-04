2d0aca87b3323c270c36778a2723efbb662067e3
Retro <44505837+dankmolot@users.noreply.github.com>
2022-02-04 13:10:11 +0200

    fixed lint error ;p

--- lua/gpm/sh_include.lua
+++ lua/gpm/sh_include.lua
@@ -2,6 +2,6 @@ local GPM = GPM
 
 GPM.SH('sh_logger.lua', 'gpm')
 GPM.SH('sh_semver.lua', 'gpm')
-GPM.SH('sh_author.lua', 'gpm') 
+GPM.SH('sh_author.lua', 'gpm')
 GPM.SH('sh_package.lua', 'gpm')
 GPM.SH('sh_loader.lua', 'gpm')
