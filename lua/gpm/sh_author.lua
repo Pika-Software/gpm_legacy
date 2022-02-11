e6e8c1e3756ac1aece594ac854beffe4b1f136bb
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-02-11 04:30:26 +0400

    Discord Tags Support

--- lua/gpm/sh_author.lua
+++ lua/gpm/sh_author.lua
@@ -1,14 +1,16 @@
 local GPM = GPM
 GPM.Author = GPM.Author or {}
 
-local function parseAuthor(str)
-	local name = string.match(str, '^(.-)%s?[<%(]') or string.match(str, '.+')
-	local email = string.match(str, '<(.-)>')
-	local url = string.match(str, '%((.-)%)')
+local function parseAuthor( str )
+	local name = str:match( '^(.-)%s?[<%(]' ) or str:match( '.+' )
+	local email = str:match( '<(.-)>' )
+	local discord = str:gsub( '%s*([%g]+)%s*(#)%s*([%d]+)%s*', '%1%2%3' )
+	local url = str:match( '%((.-)%)' )
 
 	return {
 		name = name,
 		email = email,
+		discord = discord,
 		url = url
 	}
 end
@@ -20,14 +22,16 @@ function mt:__eq(other)
 	return istable(other) and
 		self.name == other.name and
 		self.email == other.email and
+		self.discord == other.discord and
 		self.url == other.url
 end
 
 function mt:__tostring()
 	local buffer = { self.name }
 	if self.email then buffer[#buffer + 1] = ' <' .. self.email .. '>' end
+	if self.discord then buffer[#buffer + 1] = ' (' .. self.discord .. ')' end
 	if self.url then buffer[#buffer + 1] = ' (' .. self.url .. ')' end
-	return table.concat(buffer)
+	return table.concat( buffer )
 end
 
 function mt.new(v)
@@ -42,6 +46,7 @@ function mt.new(v)
 	return setmetatable({
 		name = v.name,
 		email = v.email,
+		discord = v.discord,
 		url = v.url
 	}, mt)
 end
