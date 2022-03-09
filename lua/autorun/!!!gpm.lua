dd5b91bd7f189fbd4f8c3ac8d4e3b7fc84c8d3c7
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-03-10 01:12:30 +0400

    Special for linter <3

--- lua/autorun/!!!gpm.lua
+++ lua/autorun/!!!gpm.lua
@@ -1,1 +1,36 @@
-local a,d,b="",Color,function(c)a=a.."\n\t"..c end;b("    ______     ______   __    __")b([[   /\  ___\   /\  == \ /\ "-./  \]])b([[   \ \ \__ \  \ \  _-/ \ \ \-./\ \]])b([[    \ \_____\  \ \_\    \ \_\ \ \_\]])b([[     \/_____/   \/_/     \/_/  \/_/]])local e,f,g=d(60,125,250),d(250,225,60),MsgC;g(e,a,"\n\n")local h={e,"\tGLua Package Manager by Pika Software\n\n"}local i=table.insert;local function j(k,l)i(h,f)i(h,"\t"..k..": ")i(h,e)i(h,l.."\n")end;j("GitHub","https://github.com/Pika-Software/gpm")j("Discord","https://discord.gg/3UVxhZj")j("Web Site","http://pika-soft.ru")i(h,"\n")g(unpack(h))AddCSLuaFile('gpm/sh_init.lua')include('gpm/sh_init.lua')
+local text1 = ""
+function addLine(c)
+    text1 = text1 .. "\n\t" .. c
+end
+
+addLine("    ______     ______   __    __")
+addLine([[   /\  ___\   /\  == \ /\ "-./  \]])
+addLine([[   \ \ \__ \  \ \  _-/ \ \ \-./\ \]])
+addLine([[    \ \_____\  \ \_\    \ \_\ \ \_\]])
+addLine([[     \/_____/   \/_/     \/_/  \/_/]])
+
+local color1, color2 = Color(60, 125, 250), Color(250, 225, 60)
+MsgC( color1, text1, "\n\n" )
+
+local h = {color1, "\tGLua Package Manager by Pika Software\n\n"}
+
+local insert = table.insert
+local function addTag( title, text )
+    insert( h, color2 )
+    insert( h, "\t" .. title .. ": " )
+    insert( h, color1 )
+    insert( h, text .. "\n" )
+end
+
+addTag("GitHub","https://github.com/Pika-Software/gpm")
+addTag("Discord","https://discord.gg/3UVxhZj")
+addTag("Web Site","http://pika-soft.ru")
+insert( h,"\n" )
+
+MsgC( unpack( h ) )
+
+if SERVER then
+    AddCSLuaFile('gpm/sh_init.lua')
+end
+
+include('gpm/sh_init.lua')
