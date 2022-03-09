f87a8d82540211d0b54c1b4d2c0345cdd39674f0
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-03-10 01:29:42 +0400

    Retro pls delete linter

--- lua/autorun/!!!gpm.lua
+++ lua/autorun/!!!gpm.lua
@@ -12,22 +12,20 @@ addLine([[     \/_____/   \/_/     \/_/  \/_/]])
 local color1, color2 = Color(60, 125, 250), Color(250, 225, 60)
 MsgC( color1, text1, "\n\n" )
 
-local h = {color1, "\tGLua Package Manager by Pika Software\n\n"}
-
-local insert = table.insert
+local text2, table_insert = {color1, "\tGLua Package Manager by Pika Software\n\n"}, table.insert
 local function addTag( title, text )
-    insert( h, color2 )
-    insert( h, "\t" .. title .. ": " )
-    insert( h, color1 )
-    insert( h, text .. "\n" )
+    table_insert( text2, color2 )
+    table_insert( text2, "\t" .. title .. ": " )
+    table_insert( text2, color1 )
+    table_insert( text2, text .. "\n" )
 end
 
 addTag("GitHub","https://github.com/Pika-Software/gpm")
 addTag("Discord","https://discord.gg/3UVxhZj")
 addTag("Web Site","http://pika-soft.ru")
-insert( h,"\n" )
+table_insert( text2,"\n" )
 
-MsgC( unpack( h ) )
+MsgC( unpack( text2 ) )
 
 if SERVER then
     AddCSLuaFile('gpm/sh_init.lua')
