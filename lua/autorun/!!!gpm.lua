bbf4e58d56e7c7b724ff617e768d37addd175533
PrikolMen <44779902+PrikolMen@users.noreply.github.com>
2022-04-08 22:44:39 +0300

    gLua

--- lua/autorun/!!!gpm.lua
+++ lua/autorun/!!!gpm.lua
@@ -12,7 +12,7 @@ addLine([[     \/_____/   \/_/     \/_/  \/_/]])
 local color1, color2 = Color(60, 125, 250), Color(250, 225, 60)
 MsgC( color1, text1, "\n\n" )
 
-local text2, table_insert = {color1, "\tGLua Package Manager by Pika Software\n\n"}, table.insert
+local text2, table_insert = {color1, "\tgLua Package Manager by Pika Software\n\n"}, table.insert
 local function addTag( title, text )
     table_insert( text2, color2 )
     table_insert( text2, "\t" .. title .. ": " )
