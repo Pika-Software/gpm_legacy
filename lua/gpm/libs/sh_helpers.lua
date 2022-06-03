f37d4a863425b38041f4b8c26e8845a21e501ed8
Retro <dankmolot@gmail.com>
2022-06-03 16:27:43 +0300

    NotImplementedError, ConvertPackageToID and IsDir

--- lua/gpm/libs/sh_helpers.lua
+++ lua/gpm/libs/sh_helpers.lua
@@ -1,3 +1,4 @@
+--- Helpers for many things
 module( "GPM", package.seeall )
 
 --- Packs varargs to table
@@ -38,4 +39,57 @@ end
 -- @param func
 function NextTick(func)
 	timer.Simple(0, func)
+end
+
+--- Throws "Not Implemented" error.
+function NotImplementedError()
+	error("This feature is not implemented.")
+end
+
+--- Converts package name and version
+-- @param name of package, can be also be package
+-- @param version of package
+-- @return a package id (string)
+function ConvertPackageToID( name, version )
+	-- @todo
+	if --[[ isPackage ]] false then
+		return ConvertPackageToID( name.name, name.version )
+	end
+
+	return tostring(name) .. "@" .. tostring(version)
+end
+
+--- Converts package id to name and version
+-- @param id of package
+-- @return a package name, a package version
+function ConvertIDToPackage( id )
+	local name, version = string.match(id, "([%w_-]+)@(.+)")
+
+	return name, version
+end
+
+--- Checks if given path is directory
+-- Sometimes client-side fails to determine if directory exists
+-- This functions should help with this bug
+-- @see https://github.com/Facepunch/garrysmod-issues/issues/1038
+--
+-- @param path to directory
+-- @param gamePath GAME, LUA and etc.
+-- @return true if directory exists
+function IsDir( path, gamePath )
+	if not isstring(path) or not isstring(gamePath) then return false end
+
+	-- On server-side everything work fine
+	if SERVER then
+		return file.IsDir( path, gamePath )
+	end
+
+	-- Adding wildcard to the end of path, so file.Find hack will work
+	-- path/to/dir -> path/to/dir*
+	local folders = select(2, file.Find( path .. "*", gamePath ))
+
+	-- If nothing found, directory isn't exists
+	if not folders or not folders[1] then return false end
+
+	return string.EndsWith( path, folders[1] )
 end
